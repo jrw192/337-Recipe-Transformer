50808dc61c064fb05fbbbf9ffa609dc3b45b6794
kimberlykwon <kimberlykwon2019@u.northwestern.edu>
2019-02-25 12:41:33 -0600

    updated action items

--- utilities.py
+++ utilities.py
@@ -2,3 +2,12 @@
 # File meant for helper functions
 # comment code with @param and @return with the datatype and data info 
 ###
+
+###
+# ACTION ITEMS:
+#
+# get links from website - Jodie
+# get json response from website - Jodie
+# get parsed response - Kimberly
+# get ingredients, name, descriptor, tools, quantity - Ilham, Diane
+###
