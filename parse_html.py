3e523b1e3345e66d785d752a74d4399fb5dde8a6
DianeLam <43661044+DianeLam@users.noreply.github.com>
2019-03-04 13:25:49 -0600

    Update parse_html.py

--- parse_html.py
+++ parse_html.py
@@ -9,27 +9,28 @@ from utilities import list_formatter
 # @return: dictionary - keys: part of the recipe; values: list of strings or string of contents
 ###
 def parse_html(url):
+	#added on for user input
+	
   # retrieve page contents
-  page = requests.get(url)
-  tree = html.fromstring(page.content)
+	page = requests.get(url)
+	tree = html.fromstring(page.content)
 
   # get each recipe part from xml and add to dictionary
-  recipe_parts = {} 
-  recipe_parts['ingredients'] = tree.xpath('//span[@class = "recipe-ingred_txt added"]/text()')
-  recipe_parts['directions'] = list_formatter(tree.xpath('//span[@class = "recipe-directions__list--item"]/text()')) # join into one string later?
-  recipe_parts['name'] = tree.xpath('//h1[@class = "recipe-summary__h1"]/text()')[0]
-  recipe_parts['type'] = list_formatter(tree.xpath('//span[@class = "toggle-similar__title"]/text()'))[2:] # first two are 'home' and 'recipe'
-  recipe_parts['time'] = tree.xpath('//span[@class = "ready-in-time"]/text()')[0]
+	recipe_parts = {} 
+	recipe_parts['ingredients'] = tree.xpath('//span[@class = "recipe-ingred_txt added"]/text()')
+	recipe_parts['directions'] = list_formatter(tree.xpath('//span[@class = "recipe-directions__list--item"]/text()')) # join into one string later?
+	recipe_parts['name'] = tree.xpath('//h1[@class = "recipe-summary__h1"]/text()')[0]
+	recipe_parts['type'] = list_formatter(tree.xpath('//span[@class = "toggle-similar__title"]/text()'))[2:] # first two are 'home' and 'recipe'
+	recipe_parts['time'] = tree.xpath('//span[@class = "ready-in-time"]/text()')[0]
   # find servings
-
-  return recipe_parts
+	return recipe_parts
 
 # for testing purposes
 if __name__ == '__main__':
-  test_urls = ['https://www.allrecipes.com/recipe/223016/fresh-blueberry-cake/?internalSource=staff%20pick&referringId=17263&referringContentType=Recipe%20Hub', # cake
-              'https://www.allrecipes.com/recipe/222000/spaghetti-aglio-e-olio/?internalSource=hub%20recipe&referringContentType=Search&clickId=cardslot%203', # aglio e olio
-              'https://www.allrecipes.com/recipe/230818/pork-fried-rice/?internalSource=hub%20recipe&referringContentType=Search&clickId=cardslot%202'] # fried rice
-
-  print(parse_html(test_urls[0]))
-  print(parse_html(test_urls[1]))
-  print(parse_html(test_urls[2]))
+	test_urls = ['https://www.allrecipes.com/recipe/223016/fresh-blueberry-cake/?internalSource=staff%20pick&referringId=17263&referringContentType=Recipe%20Hub', # cake
+	'https://www.allrecipes.com/recipe/222000/spaghetti-aglio-e-olio/?internalSource=hub%20recipe&referringContentType=Search&clickId=cardslot%203', # aglio e olio
+	'https://www.allrecipes.com/recipe/230818/pork-fried-rice/?internalSource=hub%20recipe&referringContentType=Search&clickId=cardslot%202'] # fried rice
+	
+	print(parse_html(test_urls[0]))
+	print(parse_html(test_urls[1]))
+	print(parse_html(test_urls[2]))
