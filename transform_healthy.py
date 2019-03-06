cd586d89bbea8b0fbde74a9343dbf9921e0869fc
kimberlykwon <kimberlykwon2019@u.northwestern.edu>
2019-03-06 12:43:18 -0600

    transform

---
+++ transform_healthy.py
@@ -0,0 +1,31 @@
+from get_parts import parse_one_ingredient
+
+###
+# Transform ingredients to and from healthy
+#
+# @param: steps - list of strings - steps of recipe
+#         food_dict - dictionary - keys: string - healthy/unhealty, values: string - unhealthy/healthy substitute
+#         ingredients - list of strings - ingredients list for current recipe
+# @return: list of strings - transformed steps
+#          list of strings - transformed ingredients list
+###
+def transform_healthy(steps, food_dict, ingredients):
+  t_ingredients = []
+
+  # transform ingredients
+  for ingredient in ingredients:
+    ingred_name = parse_one_ingredient(ingredient)[0]
+    ingredient.replace(ingred_name, food_dict[ingred_name])
+    t_ingredients.append(food_dict[ingred_name])
+
+  # transform steps
+  for step in steps:
+    for ingredient in t_ingredients:
+      if ingredient in step:
+        step.replace(ingredient, food_dict[ingredient])
+    
+
+
+
+
+  
