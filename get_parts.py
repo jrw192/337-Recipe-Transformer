c21cbb382d32fc63ec1baf05554b2ce922babdf2
Jodie Wei <jodiewei2019@u.northwestern.edu>
2019-03-07 16:08:51 -0600

    solve for case of multiple ingredients like salt and pepper

--- get_parts.py
+++ get_parts.py
@@ -19,6 +19,7 @@ m_f.close()
 
 #takes in one ingredient listing and returns an array of name, quantity, measurement, preparation
 def parse_one_ingredient(listing):
+	split_information = []
 	# print(listing)
 	rest = listing #we will be splitting the listing several times based on found values
 
@@ -78,19 +79,29 @@ def parse_one_ingredient(listing):
 		rest = rest.split(measurement)[0]
 	elif measurement != "N/A":
 		rest = rest.split(measurement)[1]
-
 	#find name
-	name = rest[1:] # leading space
-	name = name.lstrip()
-	name = name.rstrip()
-	# print("name: %s, quantity: %s, measurement: %s, preparation: %s" % (name, quantity, measurement, preparation))
-	return [name, quantity, measurement, preparation]
+	#strip leading and trailing spaces
+	name = rest.lstrip().rstrip()
+
+	if 'and' in name:
+		name1, name2 = name.split('and')
+		name1 = name1.lstrip().rstrip()
+		name2 = name2.lstrip().rstrip()
+		split_information.append([name1, quantity, measurement, preparation])
+		split_information.append([name2, quantity, measurement, preparation])
+	else:
+		split_information.append([name, quantity, measurement, preparation]);
+
+	# print("split_information: ", split_information)
+	return split_information
 
 #takes in an array of the ingredient list
 def parse_ingredient_list(ingredients):
 	parsed_ingredients = []
 	for ingredient in ingredients:
-		parsed_ingredients.append(parse_one_ingredient(ingredient))
+		parsed = parse_one_ingredient(ingredient)
+		for item in parsed:
+			parsed_ingredients.append(item)
 
 	return parsed_ingredients
 
@@ -108,11 +119,11 @@ def parse_all_steps(steps_list):
 
 
 if __name__ == "__main__":
-	# ingredients = ['1 pound uncooked spaghetti', '6 cloves garlic, thinly sliced', '1/2 cup olive oil', '1/4 teaspoon red pepper flakes, or to taste', 'salt and freshly ground black pepper to taste', '1/4 cup chopped fresh Italian parsley', '1 cup finely grated Parmigiano-Reggiano cheese']
+	ingredients = ['1 pound uncooked spaghetti', '6 cloves garlic, thinly sliced', '1/2 cup olive oil', '1/4 teaspoon red pepper flakes, or to taste', 'salt and freshly ground black pepper to taste', '1/4 cup chopped fresh Italian parsley', '1 cup finely grated Parmigiano-Reggiano cheese']
 	steps = ['Bring a large pot of lightly salted water to a boil. Cook spaghetti in the boiling water, stirring occasionally until cooked through but firm to the bite, about 12 minutes. Drain and transfer to a pasta bowl.', 'Combine garlic and olive oil in a cold skillet. Cook over medium heat to slowly toast garlic, about 10 minutes. Reduce heat to medium-low when olive oil begins to bubble. Cook and stir until garlic is golden brown, about another 5 minutes. Remove from heat.', 'Stir red pepper flakes, black pepper, and salt into the pasta. Pour in olive oil and garlic, and sprinkle on Italian parsley and half of the Parmigiano-Reggiano cheese; stir until combined.', 'Serve pasta topped with the remaining Parmigiano-Reggiano cheese.']
-	ingredients = ["1 (18.25 ounce) package devil's food cake mix", "1 (5.9 ounce) package instant chocolate pudding mix", "1 cup sour cream", "1 cup vegetable oil", "1/2 cup warm water", "2 cups semisweet chocolate chips"]
-	parse_ingredient_list(ingredients)
-
+	# ingredients = ["1 (18.25 ounce) package devil's food cake mix", "1 (5.9 ounce) package instant chocolate pudding mix", "1 cup sour cream", "1 cup vegetable oil", "1/2 cup warm water", "2 cups semisweet chocolate chips"]
+	parsed = parse_ingredient_list(ingredients)
+	# print(parsed)
 
 
 
