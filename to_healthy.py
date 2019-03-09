998b3bad365e628e271eaf666b3810588a0ca8ae
DianeLam <43661044+DianeLam@users.noreply.github.com>
2019-03-09 14:59:28 -0600

    minor changes

--- to_healthy.py
+++ to_healthy.py
@@ -7,7 +7,7 @@ def transform_healthy(recipe):
 	newingredients=[]
 	counti=-1
 	countd=-1
-	recipe = parse_html()
+	# recipe = parse_html()
 	name=recipe['name']
 	directions=recipe['directions']
 	ingredients=recipe["ingredients"]
@@ -23,48 +23,48 @@ def transform_healthy(recipe):
 
 
 	# version 1 (also works/ don't delete)
-	# 	#iterate through the ingredients list and see if the words are in the tohealthy dict and then change it accordingly.
-	# for word in ingredients:
-	# 	counti+=1
-	# 	for subst in tohealthy:
-	# 		if subst in word:
-	# 			# newingredients.append(subst)
-	# 			word=word.replace(subst, tohealthy[subst])
-	# 			ingredients[counti]=word
-		
-	# print(ingredients)
-
-	# #iterate through the directions list and see if the words are in the tohealthy dict and then change it accordingly.
-	# for words in directions:
-	# 	countd+=1
-	# 	for ing in tohealthy:
-	# 		if ing in words:
-	# 			words=words.replace(ing,tohealthy[ing])
-	# 			directions[countd]=words
-
-	# print(directions)
-
-	# version 2(also works)
-	#iterate through the ingredients list and see if the words are in the tohealthy dict and then change it accordingly.
+		#iterate through the ingredients list and see if the words are in the tohealthy dict and then change it accordingly.
 	for word in ingredients:
 		counti+=1
 		for subst in tohealthy:
 			if subst in word:
-				newingredients.append(subst)
-				ingredients[counti]=word.replace(subst, tohealthy[subst])
+				# newingredients.append(subst)
+				word=word.replace(subst, tohealthy[subst])
+				ingredients[counti]=word
 		
 	print(ingredients)
 
 	#iterate through the directions list and see if the words are in the tohealthy dict and then change it accordingly.
 	for words in directions:
 		countd+=1
-		for ing in newingredients:
+		for ing in tohealthy:
 			if ing in words:
 				words=words.replace(ing,tohealthy[ing])
 				directions[countd]=words
 
 	print(directions)
-	
+
+	# # version 2(also works)
+	# #iterate through the ingredients list and see if the words are in the tohealthy dict and then change it accordingly.
+	# for word in ingredients:
+	# 	counti+=1
+	# 	for subst in tohealthy:
+	# 		if subst in word:
+	# 			newingredients.append(subst)
+	# 			ingredients[counti]=word.replace(subst, tohealthy[subst])
+		
+	# print(ingredients)
+
+	# #iterate through the directions list and see if the words are in the tohealthy dict and then change it accordingly.
+	# for words in directions:
+	# 	countd+=1
+	# 	for ing in newingredients:
+	# 		if ing in words:
+	# 			words=words.replace(ing,tohealthy[ing])
+	# 			directions[countd]=words
+
+	# print(directions)
+	return name, ingredients, directions
 
 
 
--- to_unhealthy.py
+++ to_unhealthy.py
@@ -7,7 +7,7 @@ def transform_unhealthy(recipe):
 	newingredients=[]
 	counti=-1
 	countd=-1
-	recipe = parse_html()
+	# recipe = parse_html()
 	name=recipe['name']
 	directions=recipe['directions']
 	ingredients=recipe["ingredients"]
@@ -23,48 +23,49 @@ def transform_unhealthy(recipe):
 
 
 	# version 1 (also works/ don't delete)
-	# 	#iterate through the ingredients list and see if the words are in the tounhealthy dict and then change it accordingly.
-	# for word in ingredients:
-	# 	counti+=1
-	# 	for subst in tounhealthy:
-	# 		if subst in word:
-	# 			# newingredients.append(subst)
-	# 			word=word.replace(subst, tounhealthy[subst])
-	# 			ingredients[counti]=word
-		
-	# print(ingredients)
-
-	# #iterate through the directions list and see if the words are in the tounhealthy dict and then change it accordingly.
-	# for words in directions:
-	# 	countd+=1
-	# 	for ing in tounhealthy:
-	# 		if ing in words:
-	# 			words=words.replace(ing,tounhealthy[ing])
-	# 			directions[countd]=words
-
-	# print(directions)
-
-	# version 2(also works)
-	#iterate through the ingredients list and see if the words are in the tounhealthy dict and then change it accordingly.
+		#iterate through the ingredients list and see if the words are in the tounhealthy dict and then change it accordingly.
 	for word in ingredients:
 		counti+=1
 		for subst in tounhealthy:
 			if subst in word:
-				newingredients.append(subst)
-				ingredients[counti]=word.replace(subst, tounhealthy[subst])
+				# newingredients.append(subst)
+				word=word.replace(subst, tounhealthy[subst])
+				ingredients[counti]=word
 		
 	print(ingredients)
 
 	#iterate through the directions list and see if the words are in the tounhealthy dict and then change it accordingly.
 	for words in directions:
 		countd+=1
-		for ing in newingredients:
+		for ing in tounhealthy:
 			if ing in words:
 				words=words.replace(ing,tounhealthy[ing])
 				directions[countd]=words
 
 	print(directions)
+
+	# version 2(also works)
+	# #iterate through the ingredients list and see if the words are in the tounhealthy dict and then change it accordingly.
+	# for word in ingredients:
+	# 	counti+=1
+	# 	for subst in tounhealthy:
+	# 		if subst in word:
+	# 			newingredients.append(subst)
+	# 			ingredients[counti]=word.replace(subst, tounhealthy[subst])
+		
+	# print(ingredients)
+
+	# #iterate through the directions list and see if the words are in the tounhealthy dict and then change it accordingly.
+	# for words in directions:
+	# 	countd+=1
+	# 	for ing in newingredients:
+	# 		if ing in words:
+	# 			words=words.replace(ing,tounhealthy[ing])
+	# 			directions[countd]=words
+
+	# print(directions)
 	
+	return name, ingredients, directions
 
 
 
